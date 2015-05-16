ffe6a819d7d41169e99d18c52f18aefa09bff9a4
phi <phi1618jp@gmail.com>
2015-05-17 05:55:27 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # learnstant
 学ぶ(learn) + instant = learnstant.com
+
+## 内容
+
+- Training
+	- １行にフォーカス
+	- Q/A
+- Application
+	- 制作の流れ, 全体にフォーカス
+- Tips
+- Reference
+
+## ジャンル
+
+- HTML
+- CSS
+- CSS3
+- JavaScript
+- tmlib.js
+
---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+angular.module('todoApp', []).controller('ItemListController', function() {
+
+  var todoList = this;
+  todoList.items = [
+    {
+      name: '変数を学ぼう',
+      list: [
+        {name:1, link:'hoge.html'},
+        {name:2, link:'hoge.html'},
+        {name:3, link:'hoge.html'},
+      ]
+    },
+    {
+      name: 'if 文を学ぼう',
+      list: [
+        {name:1, link:'hoge.html'},
+        {name:2, link:'hoge.html'},
+        {name:3, link:'hoge.html'},
+      ]
+    },
+  ];
+
+});
+
+window.onload = function() {
+  $(".button-collapse").sideNav();
+};
