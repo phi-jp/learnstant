0ab372bb1f24d8e560919cb77c179b8bccb61a61
phi <phi1618jp@gmail.com>
2015-05-17 07:09:13 +0900

    fix list

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,15 @@
-angular.module('todoApp', []).controller('ItemListController', function() {
+angular.module('App', []).controller('ItemListController', function($scope) {
 
-  var todoList = this;
-  todoList.items = [
+  var CategoryList = this;
+  CategoryList.items = [
     {
+      id: 'var',
       name: '変数を学ぼう',
       list: [
-        {name:1, link:'http://goo.gl/KMf0lh'},
-        {name:2, link:'http://goo.gl/zrPq4d'},
-        {name:3, link:'hoge.html'},
-      ]
+        {name:'hoge', link:'http://goo.gl/KMf0lh'},
+        {name:'foo', link:'http://goo.gl/zrPq4d'},
+        {name:'bar', link:'hoge.html'},
+      ],
     },
     {
       name: 'if 文を学ぼう',
@@ -16,9 +17,41 @@ angular.module('todoApp', []).controller('ItemListController', function() {
         {name:1, link:'hoge.html'},
         {name:2, link:'hoge.html'},
         {name:3, link:'hoge.html'},
-      ]
+      ],
     },
   ];
+
+
+  $scope.init = function() {
+    var hash = location.hash.substr(1);
+    if (!hash) return ;
+
+    var params = hash.split('/');
+    var categoryId = params[0];
+    var itemId = params[1];
+
+    var category = CategoryList.items.filter(function(elm) {
+      return elm.id == categoryId || elm.name == categoryId;
+    })[0];
+    category.active = true;
+
+    var item = category.list.filter(function(elm) {
+      return elm.id == itemId || elm.name == itemId;
+    })[0];
+    item.active = true;
+  };
+
+  $scope.click = function(item, elm) {
+    var itemId = item.id || item.name;
+    var elmId = elm.id || elm.name;
+
+    location.hash = itemId + '/' + elmId;
+
+    item.list.forEach(function(elm) {
+      elm.active = false;
+    });
+    elm.active = true;
+  };
 
 });
 
